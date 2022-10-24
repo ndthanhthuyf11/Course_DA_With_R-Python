d19f3471679af17128166edef4d642306db150cd
Nguyen Do Thanh Thuy <thuyndt19406c@st.uel.edu.vn>
2022-10-25 00:10:54 +0700

    buoi 2 covid api

---
+++ TroGiang/Buoi2/api.py
@@ -0,0 +1,12 @@
+import pip
+pip install requests
+# import requests
+
+# url = "https://api.covidtracking.com/v1/us/daily.json"
+
+# payload={}
+# headers = {}
+
+# response = requests.request("GET", url, headers=headers, data=payload)
+
+# print(response.text)
